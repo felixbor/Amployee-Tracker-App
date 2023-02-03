6d615fe69838196ebcd8d05fa8f57cf3d9603df6
felixbor <felixbor@gmail.com>
2023-02-02 22:58:06 -0400

    fixed bugs in schema

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const DbConnection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: 'root',
-  password: 'yourPassword',
+  password: 'Crbkta',
   database: 'company_db',
 }
 );
